eb4ab635e423be2f4bdf1557179e0bd0e86913a0
MingHsui <145117810+minghsuihuang@users.noreply.github.com>
2023-12-24 19:34:20 +0800

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 # web.py
 import streamlit as st
-from model import RNN
+from model import Model
 import torch
 import pickle
 
@@ -9,14 +9,14 @@ with open('token_to_num.pkl', 'rb') as f:
     token_to_num = pickle.load(f)
 
 # 加載模型
-model = RNN(embedding_dim=256, hidden_size=64, num_layer=2, token_to_num=token_to_num)
+model = Model(embedding_dim=256, hidden_size=64, num_layer=2, token_to_num=token_to_num)
 model.load_state_dict(torch.load('best_model.pt', map_location=torch.device('cpu')))
 model.eval()
 
 st.title('情感分析模型')
 
 # 文本輸入
-review = st.text_area("輸入您的評論：")
+review = st.text_area("輸入您要判斷的字句：")
 
 if st.button('預測'):
     # 進行預測
